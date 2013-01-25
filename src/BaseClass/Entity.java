46c4f3df551c60f5a8387d2f933a9ece5dd12ad3
Moinak Bhattacharyya <mb.totaldomination@gmail.com>
2013-01-24 21:56:43 -0800

    Dev V0.2

--- src/BaseClass/Entity.java
+++ src/BaseClass/Entity.java
@@ -5,11 +5,35 @@ public class Entity {
 	/**
 	 * @param args
 	 */
-	int[][] movemat=null;
-	public Entity(int[][] movemat) {
+	private double[][] movemat=null;
+	private int[] factorials={1,1,2,6,24,120,720,5040};
+	public Entity(double[][] movemat) {
 		// TODO Auto-generated constructor stub
 		this.movemat=movemat;
 	}
-	public int[] get(int ind); 
+	public double[] get(int ind){
+		return movemat[ind];
+	}
+	public void update(int delt){
+		for (int i = 0; i < movemat.length; i++) {
+			double[] arr=movemat[i];
+			for (int j = 0; j < arr.length; j++) {
+				arr=updateind(arr, j, delt);
+			}
+		}
+	}
+	private double[] updateind(double[] list, int ind, int t){
+		for (int i = ind; i < list.length&&i<ind+8; i++) {
+			list[ind]+=(1/factorials[i-ind])*pow(t,i-ind)*list[i];
+		}
+		return list;
+	}
+	private double pow(double base, double pow){
+		double res=1;
+		for (int i = 0; i < pow; i++) {
+			res*=base;
+		}
+		return res;
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,33 @@ import java.nio.ByteBuffer;
 import org.lwjgl.Sys;
 import org.lwjgl.opengl.GL11;
 
+import BaseClass.Timer;
+import Exceptions.NotStartedException;
+
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws NotStartedException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NotStartedException, InterruptedException {
 		// TODO Auto-generated method stub
 		Sys.openURL("google.com");
+		printarr(new int[][]{{1,2},{1}});
+		Timer time=new Timer();
 		
+		
+		
+	}
+	public static void printarr(int[][] arr){
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[j].length; j++) {
+				System.out.print(arr[i][j]+",");
+			}
+			System.out.println();
+		}
 	}
 
 }
